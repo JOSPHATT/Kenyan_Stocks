eafeb96d370dca9719ad87f853699ddc0ac98f0c
JOSPHATT <kimwelemutunga@gmail.com>
2024-08-29 11:27:44 +0300

    Update Main.py
    Added news feeds function from kenyan _stocks_news_FEEDS.ipynb

--- Main.py
+++ Main.py
@@ -64,15 +64,75 @@ def final_data():
 time_stamp=str(time.asctime())
 Final_data={}  
 Final_data[time_stamp]=final_data()
-filename = '3hrly_kenyanstocks_prices.json'
-entry = final_data()
+#filename = '3hrly_kenyanstocks_prices.json'
+#entry = final_data()
+print(Final_data)
+#########################################################################################################################
+##BUSINESS NEWS FEEDS FUNCTION
 
-if os.stat(filename).st_size == 0:
-    with open(filename, "w") as file:
-        json.dump(Final_data, file)
-else:
-    with open(filename, "r+") as file:
-        data = json.loads(file)
-        data[time_stamp]=entry
-        file.seek(0)
-        json.dump(data, file)
+# prompt1: code to extract news from  https://live.mystocks.co.ke/news.php
+import requests
+from bs4 import BeautifulSoup
+
+url1 = 'https://live.mystocks.co.ke/news.php'
+response1 = requests.get(url1)
+
+soup1 = BeautifulSoup(response1.content, 'html.parser')
+news_articles = soup1.find_all('a', target="_blank")
+news_publisher = soup1.find_all('i', {'class':"nPubl"})
+news_date = soup1.find_all('i', {'class':"nDate"})
+
+headline = []
+publisher = []
+dates = []
+for article in news_articles:
+    headline.append(article.text)
+for pub in news_publisher:
+    publisher.append(pub.text)
+for dat in news_date:
+    dates.append(dat.text)
+#print(headline)
+#print(publisher)
+#print(dates)
+kenyan_stocks_news = list(zip(headline,zip(publisher,dates)))
+
+# prompt2: code to extract news from  'https://www.theeastafrican.co.ke/tea/business'
+url2 = 'https://www.theeastafrican.co.ke/tea/business'
+response2 = requests.get(url2)
+soup2 = BeautifulSoup(response2.content, 'html.parser')
+east_african_news_articles =soup2.find_all('div', {'class':"five-eight column"})
+east_african_news_articles_others = soup2.find_all('div', {'class':"sidebar-item"})
+the_east_african_news_articles=east_african_news_articles_others+east_african_news_articles
+the_east_african_news=[]
+for article in the_east_african_news_articles:
+    the_east_african_news.append(article.text)
+
+# prompt3: code to extract news from  https://kenyanwallstreet.com/
+url3 = 'https://kenyanwallstreet.com/'
+response3 = requests.get(url3)
+soup3 = BeautifulSoup(response3.content, 'html.parser')
+kenyan_wall_street_news_articles =soup3.find_all('div', {'class':"jeg_block_container"})
+kenyan_wall_street_news_articles_list=[]
+for article in kenyan_wall_street_news_articles:
+  kenyan_wall_street_news_articles_list.append(article.text)
+kenyan_wall_street_news=kenyan_wall_street_news_articles_list
+
+# prompt4: code to extract news from  https://www.businessdailyafrica.com/
+url4 = 'https://www.businessdailyafrica.com/'
+response4 = requests.get(url4)
+
+soup4 = BeautifulSoup(response4.content, 'html.parser')
+business_daily_news_articles =soup4.find_all('div', {'class':"grid-container-bd"})
+business_daily_news=[]
+for article in business_daily_news_articles:
+  s=article.text
+  s1= s.split('\n')
+  for i in s1:
+    if i=='':
+      s1.remove(i)
+    else:
+      business_daily_news.append(i)
+
+Collected_Business_News=kenyan_stocks_news+business_daily_news+the_east_african_news+kenyan_wall_street_news
+print(Collected_Business_News)
+#####################################################################################################################################
