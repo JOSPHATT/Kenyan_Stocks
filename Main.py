43082e724a06391796c6fe6e451879d81d4342c1
JOSPHATT <kimwelemutunga@gmail.com>
2024-09-21 00:08:29 +0300

    Update Main.py
    solved update dictionary error (line 103)

--- Main.py
+++ Main.py
@@ -100,7 +100,7 @@ def append_json(new_data, filename):
           # First we load existing data into a dict.
     file_data = json.load(file)
         # Join new_data with file_data inside emp_details
-    file_data(new_data)
+    file_data.update(new_data)
         # Sets file's current position at offset.
     file.seek(0)
         # convert back to json.
